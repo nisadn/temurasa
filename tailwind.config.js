29c0c0892a3e983dbdb2cd947893e8a49ca0366f
fknraini <fikanuraini01@gmail.com>
2022-08-26 18:43:03 +0700

    slicing foods page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#3E86F5',
+        },
+        white: {
+          DEFAULT: '#FFFFFF',
+          200: '#F7F7F7',
+          300: '#D6D6D6',
+          400: '#808080',
+        }
+      },
+    },
   },
   plugins: [],
 }
